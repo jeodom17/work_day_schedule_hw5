84d197f96f4f75411fdf0737ebd31545290f4c5f
mac <jeodom17@gmail.com>
2021-09-24 00:28:49 -0400

    added javascript for sec coloring and add function for local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,36 +5,50 @@ function displayDate() {
     dateDisplayEl.text(toDay);
   }
 
+var curHour =  moment().format('H');
+var textBar = $('textarea');
+var saveBtn = $('saveBTN');
+
+textBar.each(function(i){
+    if (curHour > i+9) {$(this).addClass('past')}
+    if (curHour == i+9) {$(this).addClass('present')}
+    if (curHour < i+9) {$(this).addClass('future')}
+})
+
+$('saveBtn').on('click' , storeLocal);
 
 
+function storeLocal() {
 
-
-
-
-
-
-
-
-
-
-
-
-
-function init(){
-    allStorage()
-
-  function allStorage() {
-      debugger;
-      var values = [],
-         keys = Object.keys(localStorage),
-         i = keys.length;
-
-      while ( i-- ) {
-          values.push(localStorage.getItem(keys[i]));
-      }   
-      return values;
-  }
+    $(this).parent().siblings('textarea').each(function(i){
+        useInput[i]-$(this).val()
+        console.log(this)
+    })
+    console.log(useInput)
 }
+
+
+
+
+
+
+
+
+// function init(){
+//     allStorage();
+
+//   function allStorage() {
+//       debugger;
+//       var values = [],
+//          keys = Object.keys(localStorage),
+//          i = keys.length;
+
+//       while (i--) {
+//           values.push(localStorage.getItem(keys[i]));
+//       }   
+//       return values;
+//   }
+// }
 
   setInterval(displayDate, 1000);
 
