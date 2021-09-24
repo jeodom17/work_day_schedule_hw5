e5b49ed9be653dc7061e41e44b4e4b879737feca
mac <jeodom17@gmail.com>
2021-09-23 21:06:59 -0400

    added html classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,58 @@
 var dateDisplayEl = $('#currentDay');
 
 function displayDate() {
-    var rightNow = moment().format("dddd, MMMM Do, YYYY");
-    dateDisplayEl.text(rightNow);
+    var toDay = moment().format("dddd, MMMM Do, YYYY");
+    dateDisplayEl.text(toDay);
   }
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+function init(){
+    allStorage()
+
+  function allStorage() {
+      debugger;
+      var values = [],
+         keys = Object.keys(localStorage),
+         i = keys.length;
+
+      while ( i-- ) {
+          values.push(localStorage.getItem(keys[i]));
+      }   
+      return values;
+  }
+}
+
   setInterval(displayDate, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
