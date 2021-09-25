bfd6195e89258ce7ac9277cb0bb50341fe07fca4
mac <jeodom17@gmail.com>
2021-09-24 21:46:29 -0400

    finalize javascript local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,37 @@
-var dateDisplayEl = $('#currentDay');
+var dateDisplayEl = $("#currentDay");
 
 function displayDate() {
-    var toDay = moment().format("dddd, MMMM Do, YYYY");
-    dateDisplayEl.text(toDay);
-  }
-
-var curHour =  moment().format('H');
-var textBar = $('textarea');
-var saveBtn = $('saveBTN');
-
-textBar.each(function(i){
-    if (curHour > i+9) {$(this).addClass('past')}
-    if (curHour == i+9) {$(this).addClass('present')}
-    if (curHour < i+9) {$(this).addClass('future')}
-})
-
-$('saveBtn').on('click' , storeLocal);
-
-
-function storeLocal() {
-
-    $(this).parent().siblings('textarea').each(function(i){
-        useInput[i]-$(this).val()
-        console.log(this)
-    })
-    console.log(useInput)
+  var toDay = moment().format("dddd, MMMM Do, YYYY");
+  dateDisplayEl.text(toDay);
 }
 
+var curHour = moment().format("H");
+var textBar = $("textarea");
+var saveBtn = $("saveBTN");
 
+textBar.each(function (i) { 
+  var input = $(this).attr('id')
+  if (curHour > i + 9) {
+    $(this).addClass("past");
+  }
+  if (curHour == i + 9) {
+    $(this).addClass("present");
+  }
+  if (curHour < i + 9) {
+    $(this).addClass("future");
+  }
+  $(this).val(localStorage.getItem(input))
+});
 
+$(".saveBtn").on("click", storeLocal);
 
-
-
-
+function storeLocal() {
+  var textBar = $(this).parent().siblings("textarea").val();
+  var input = $(this).parent().siblings("textarea").attr('id');
+  localStorage.setItem(input, textBar)
+  console.log(textBar);
+  console.log(input)
+}
 
 // function init(){
 //     allStorage();
@@ -45,28 +44,9 @@ function storeLocal() {
 
 //       while (i--) {
 //           values.push(localStorage.getItem(keys[i]));
-//       }   
+//       }
 //       return values;
 //   }
 // }
 
-  setInterval(displayDate, 1000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+setInterval(displayDate, 1000);
